f34a5363c2e6b93d2dfef5edecbc669c4ced6456
David Bat <david_bat@live.com>
2023-01-13 20:58:01 -0300

    segundo save

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Header } from "./componentes/Header";
+import 'boxicons';
+import {BrowserRouter, RouterProps, Route} from "react-router-dom";
+import {Paginas} from "./componentes/Paginas";
+import { DataProvider }from "./context/DataProvider";
+import { Carrito } from "./componentes/Carrito";
+
+
+function App() {
+  return (
+    <DataProvider >
+    <div className="App">
+      <BrowserRouter>
+      <Header />
+      <Carrito />
+      <Paginas />
+      </BrowserRouter>
+    </div>
+    </DataProvider>
+  );
+}
+
+export default App;
